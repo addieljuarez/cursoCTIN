3e2ec4886d13eb82528803cd34834131685aa60c
Addiel <Addiel@mpmirandaMBP.local>
2013-06-05 12:42:55 -0500

    blur

--- Resources/ui/handheld/ApplicationWindow.js
+++ Resources/ui/handheld/ApplicationWindow.js
@@ -35,11 +35,15 @@ function ApplicationWindow() {
 		borderColor:'red',
 		borderWidth:2,
 		top:5,
+		keyboardType: Ti.UI.KEYBOARD_EMAIL,
+		returnKeyType:Titanium.UI.RETURNKEY_NEXT
 	});
+	
+	
 	
 	var textField2 = Titanium.UI.createTextField({
 		backgroundColor:'pink',
-		height:30,
+		height:'30dp',
 		width:200,
 		hintText:'password',
 		borderRadius:5,
@@ -47,13 +51,54 @@ function ApplicationWindow() {
 		borderWidth:2,
 		top :50,
 		passwordMask:true,
+		returnKeyType:Titanium.UI.RETURNKEY_DONE,
 	});
 	
-	contendorI.add(textField1, textField2);
+	contendorI.add(textField1);
+	contendorI.add(textField2)
+	
+	var login = Titanium.UI.createButton({
+		top:20,
+		height:30,
+		width:150,
+		title:'login',
+	});
+	
+	var registro = Titanium.UI.createButton({
+		top:70,
+		height:30,
+		width:150,
+		title:'registro',
+	});
+	
+	contenedorBoton.add(login, registro);
 	
 	self.add(contendorI);
 	self.add(contenedorBoton);
 	
+	
+	
+	//-----------------------------------------------------------------------
+	
+	textField1.addEventListener('return', function(){
+		textField2.focus();
+	});
+	
+	textField2.addEventListener('return', function(){
+		alert('bienvenido');
+	});
+	
+	login.addEventListener('click', function(){
+		
+		textField2.blur();
+		textField1.blur();
+		alert('bienvenido');
+	});
+	
+	registro.addEventListener('click', function(){
+		textField2.blur();
+		textField1.blur();
+	});
 	
 	return self;
 }

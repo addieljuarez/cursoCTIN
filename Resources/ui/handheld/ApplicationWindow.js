1c6ff4c962b36bcf9d85805aa011de8c78083412
Addiel <Addiel@mpmirandaMBP.local>
2013-06-05 11:06:25 -0500

    test 1

--- Resources/ui/handheld/ApplicationWindow.js
+++ Resources/ui/handheld/ApplicationWindow.js
@@ -1,19 +1,13 @@
-//Application Window Component Constructor
+
 function ApplicationWindow() {
-	//load component dependencies
-	var FirstView = require('ui/common/FirstView');
-		
-	//create component instance
-	var self = Ti.UI.createWindow({
-		backgroundColor:'#ffffff'
+	
+	var self = Titanium.UI.createWindow({
+		backgroundColor:'red',
+		height:100,
 	});
-		
-	//construct UI
-	var firstView = new FirstView();
-	self.add(firstView);
+	
 	
 	return self;
 }
 
-//make constructor function the public component interface
 module.exports = ApplicationWindow;

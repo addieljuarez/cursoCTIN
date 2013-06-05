40264bfab4ac60366626545fc95d3311c83b45f2
Addiel <Addiel@mpmirandaMBP.local>
2013-06-05 14:58:44 -0500

    url

--- Resources/ui/common/webView.js
+++ Resources/ui/common/webView.js
@@ -3,6 +3,40 @@ function WebView(){
 		backgroundColor :'#345678',
 	});
 	
+	// var web  = Titanium.UI.createWebView({
+		// backgroundColor:'red',
+		// height:250,
+		// width:200,
+		// url:'https://google.com'
+	// });
+	
+	// var web  = Titanium.UI.createWebView({
+		// backgroundColor:'red',
+		// height:250,
+		// width:200,
+		// url:'/ui/common/index.html',
+	// });
+	
+	var link = '-LmPLcnWBvs'
+	
+	var web  = Titanium.UI.createWebView({
+		backgroundColor:'red',
+		height:250,
+		width:200,
+		url:'http://youtube.com/embed/'+link,
+	});
+	
+	var salir = Titanium.UI.createButton({
+		bottom:50,
+		height:30,
+		width:150,
+		title:'salir',
+	})
+	
+	
+	
+	self.add(salir);
+	self.add(web);
 	return self;
 }
 module.exports = WebView;

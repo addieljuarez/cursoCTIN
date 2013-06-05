955ab52359edc8a0d616c29b99e01e28fc5bc35b
Addiel <Addiel@mpmirandaMBP.local>
2013-06-05 11:39:23 -0500

    views

--- Resources/ui/handheld/ApplicationWindow.js
+++ Resources/ui/handheld/ApplicationWindow.js
@@ -3,8 +3,32 @@ function ApplicationWindow() {
 	
 	var self = Titanium.UI.createWindow({
 		backgroundColor:'red',
-		height:100,
+		height:'100%',
+		width:'100%',
+		//layout:'horizontal',
+		layout:'vertical',
 	});
+	
+	var contendorI = Titanium.UI.createView({
+		backgroundColor:'#000',
+		height:100,
+		width:250,
+		top:20,
+		//top:300,
+	});
+	
+	var contenedorBoton = Titanium.UI.createView({
+		backgroundColor:'#fff',
+		height:150,
+		width:250,
+		top:20,
+	});
+	
+	
+	
+	
+	self.add(contendorI);
+	self.add(contenedorBoton);
 	
 	
 	return self;
